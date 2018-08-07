b5b46ee4483889c68ed73a9250b66c0c1936e37b
aboud20-meet <aboud20@meet.mit.edu>
2018-08-07 12:10:30 +0300

    hi

--- exercises/knowledge_databases.py
+++ exercises/knowledge_databases.py
@@ -10,14 +10,14 @@ session = DBSession()
 
 def add_subject(subject,topic,rating):
 
-	knowledge_object=Knowledge(
-		# knowledge_id=knowledge_id,
-		subject=subject,
-		topic=topic,
-		rating=rating)
-	session.add(knowledge_object)
-	session.commit()
-add_subject("eggplants","eggplants" , 1)
+    knowledge_object=Knowledge(
+        # knowledge_id=knowledge_id,
+        subject=subject,
+        topic=topic,
+        rating=rating)
+    session.add(knowledge_object)
+    session.commit()
+# add_subject("potatoes","potatoes" , 10)
 
 def query_all_articles():
    """
@@ -34,26 +34,53 @@ print(query_all_articles())
 def query_article_by_topic(topic):
    """
    Find the first student
-   in the database, by their name
+   in the database, by knowledge = session.query(
+      Knowledge).all()
+   return knowledge
+their name
    """
    knowledge = session.query(
        Knowledge).filter_by(
        topic=topic).first()
    return knowledge
-print(query_article_by_topic("potatoes"))
+# print(query_article_by_topic("potatoes"))
 def query_article_by_rating(rating):
-	knowledge = session.query(
-		Knowledge).filter_by(
-		rating=rating).first()
-	return knowledge
+    knowledge = session.query(
+        Knowledge).filter_by(
+        rating=rating).first()
+    return knowledge
 print(query_article_by_rating(1))
+# knowledge = session.query(
+#       Knowledge).all()
+#    return knowledge
 
 
-def delete_article_by_topic():
-	pass
+def delete_article_by_topic(topic):
+   """
+   Delete all students with a
+   certain name from the database.
+   """
+   session.query(Knowledge).filter_by(
+       topic=topic).delete()
+   session.commit()
+
+
+# delete_article_by_topic("eggplants")
+
 
 def delete_all_articles():
-	pass
+    session.query( Knowledge).delete()
+    session.commit()
+delete_all_articles()
+add_subject("potatoes","potatoes" , 10)
+print(query_all_articles())
 
-def edit_article_rating():
-	pass
+def edit_article_rating(topic,rating):
+    knowledge_object = session.query(
+    Knowledge).filter_by( 
+    topic=topic).first()
+    knowledge_object.rating = rating
+    session.commit()
+edit_article_rating("potatoes",9)
+print(query_all_articles())
+
